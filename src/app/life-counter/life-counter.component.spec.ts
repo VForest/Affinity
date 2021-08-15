355ed7a9094738e21cf923b8a8e9396c5866abdc
Vincent Forest <vincent.forest.96@gmail.com>
2021-08-15 19:49:05 -0400

    Created life-counter.component

---
+++ src/app/life-counter/life-counter.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LifeCounterComponent } from './life-counter.component';
+
+describe('LifeCounterComponent', () => {
+  let component: LifeCounterComponent;
+  let fixture: ComponentFixture<LifeCounterComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LifeCounterComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LifeCounterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
