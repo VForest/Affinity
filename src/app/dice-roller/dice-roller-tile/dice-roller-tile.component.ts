e44c3e3138acb77f293c0429414c766ef5fc22d4
Vincent Forest <vincent.forest.96@gmail.com>
2021-08-16 20:09:48 -0400

    Changed theming and added again for when result is the same

--- src/app/dice-roller/dice-roller-tile/dice-roller-tile.component.ts
+++ src/app/dice-roller/dice-roller-tile/dice-roller-tile.component.ts
@@ -9,12 +9,17 @@ export class DiceRollerTileComponent implements OnInit {
   @Input() title = '';
   @Input() faces = 0;
   result: number | string = '';
+  sameResult = false;
 
   constructor() {}
 
   ngOnInit(): void {}
 
   roll(faces: number) {
+    let oldResult = this.result;
     this.result = Math.floor(Math.random() * faces + 1);
+    oldResult === this.result
+      ? (this.sameResult = true)
+      : (this.sameResult = false);
   }
 }
